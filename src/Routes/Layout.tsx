10ab0ef49a246fcfcd44e9e39e2efd90b808696d
NighTMaReEe53 <yousefswagger1337@gmail.com>
2025-07-27 02:27:50 +0300

    first commit

--- src/Routes/Layout.tsx
+++ src/Routes/Layout.tsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom";
 import Nav from "../Components/Navbar/Nav";
 import Drawer from "../Components/UI/Drawer/Drawer";
 import Footer from "../Components/Footer/Footer";
+import AudioPlayer from "react-h5-audio-player";
 
 const Layout = () => {
   return (
@@ -11,6 +12,7 @@ const Layout = () => {
 
       <Outlet />
       <Footer />
+      <AudioPlayer className="audio" autoPlay loop src="../../public/audio.MP3" />
     </>
   );
 };
